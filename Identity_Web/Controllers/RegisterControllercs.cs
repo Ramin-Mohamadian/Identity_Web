0d45e2bf1bd870a014a6506b3e9c1796cdd1819b
Ramin-Mohamadian <raminmohamadian2020@gmail.com>
2024-07-01 11:16:55 -0700

    Write Register DTOs and Action

---
+++ Identity_Web/Controllers/RegisterControllercs.cs
@@ -0,0 +1,26 @@
+﻿using Identity_Web.Data.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity_Web.Controllers
+{
+    public class RegisterControllercs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel register)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Identity_Web/Data/DTOs/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity_Web.Data.DTOs
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage ="لطفا {0} را وارد کنید")]
+        public string  FirstName { get; set; }=string.Empty;
+
+
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string LastName { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Password { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [Compare(nameof(Password))]
+        public string PasswordConfirm { get; set; } = string.Empty;
+    }
+}
