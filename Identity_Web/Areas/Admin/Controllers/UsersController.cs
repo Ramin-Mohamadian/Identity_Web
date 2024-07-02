b80cffc7f79a671a40e139552b7af3ed1f7f16d9
Ramin-Mohamadian <raminmohamadian2020@gmail.com>
2024-07-02 10:58:23 -0700

    Make ListUserAction For Admin

---
+++ Identity_Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Identity_Web.Areas.Admin.Models.DTOs;
+using Identity_Web.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity_Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users=_userManager.Users.Select(p=> new UserListDto()
+            {
+                Id = p.Id,
+                UserName = p.UserName,
+                FirstName=p.Name,
+                LastName=p.Family,
+                PhoneNumber=p.PhoneNumber,
+                EmailConfirmed=p.EmailConfirmed,
+                AccessFaildCount = p.AccessFailedCount
+            }).ToList();
+
+
+            return View(users);
+        }
+    }
+}
---
+++ Identity_Web/Areas/Admin/Models/DTOs/UserListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Identity_Web.Areas.Admin.Models.DTOs
+{
+    public class UserListDto
+    {
+        public string Id { get; set; }=string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public bool EmailConfirmed { get; set; }
+        public int AccessFaildCount { get; set; }
+    }
+}
